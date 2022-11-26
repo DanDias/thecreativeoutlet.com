4954ce40bb1293a8d80c2d090a7283ae06d581ff
DanDias <dan.j.dias@gmail.com>
2022-11-26 02:15:12 -0800

    partial feat: groundwork for routing

--- src/index.js
+++ src/index.js
@@ -6,18 +6,33 @@ import reportWebVitals from './reportWebVitals';
 import { 
   BrowserRouter as Router,
   Route,
-  Switch,
-  Link } 
+  Routes} 
   from 'react-router-dom';
+import LargeTextLayout from './components/LargeTextLayout';
+import urize from './utils/urize';
+import config from '../package.json';
+
+const { siteData } = config;
+
+const dynamicRoutes = siteData.projects.map(({name, category}) => {
+  const path = urize([category,name]);
+  return <Route key={path} path={path} element={<div>{path}</div>} />;
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <Router>
-      <Route path='/'>
-        <App />
-      </Route>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/test' element={
+          <LargeTextLayout text="Awkward...">
+          <p>Hello. You've reached my test page. I never know what weird junk is going to be here. So... good luck!</p>
+        </LargeTextLayout>
+        } />
+        {dynamicRoutes}
+      </Routes>
     </Router>
   </React.StrictMode>
 );
